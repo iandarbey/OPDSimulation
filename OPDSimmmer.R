f2803ef13565523a07e71e1d393d9c4c5e474321
iandarbey <42093131+iandarbey@users.noreply.github.com>
2019-12-22 11:04:44 +0000

    Parameter setting to top of script and section headers

--- OPDSimmmer.R
+++ OPDSimmmer.R
@@ -3,16 +3,36 @@ library(simmer.plot)
 library(parallel)
 library(tidyverse)
 
+##OPD Parameters
 
-env <- simmer("OPD")
-
+#Patient Review Time
 new_doc_time <- 10
 new_cons_time <- 3
 return_doc_time <- 5
 return_cons_time <- 3
 dressing_time <- 20
+#Patient Priority
+new_priority <- 10
+return_priority <- 10
+dressing_prirority <- 10
+#Clinic Session Details
+OPD_Clinic_Time <- 160
+new_patient_first_slot <- 3
+return_patient_first_slot <- 3
+number_of_trials <- 100
+#Resources
+Doctors <- 4
+Nurses <- 2
+Consultants <- 1
+Administrators <- 1
 
 
+
+#Setup Environment for Simulation
+env <- simmer("OPD")
+
+
+#Make a Dressing Patient Pathway
 Dressing_Patient <- trajectory("dressing patients' path") %>%
   ## add an dressing activity 
   seize("nurse", 1) %>%
@@ -23,6 +43,8 @@ Dressing_Patient <- trajectory("dressing patients' path") %>%
   timeout(function() rnorm(1, 1)) %>%
   release("administration", 1)
 
+
+#Make a New Patient Pathway
 New_Patient <- trajectory("new patients' path") %>%
   ## add a doctor activity
   seize("doctor", 1) %>%
@@ -38,6 +60,7 @@ New_Patient <- trajectory("new patients' path") %>%
   release("administration", 1)
 
 
+#Make a Return Patient Pathway
 Return_Patient <- trajectory("return patients' path") %>%
   ## add a consultation activity
   seize("doctor", 1) %>%
@@ -52,47 +75,34 @@ Return_Patient <- trajectory("return patients' path") %>%
   timeout(function() rnorm(1, 1)) %>%
   release("administration", 1)
 
-# env %>%
-#   add_resource("nurse", 3) %>%
-#   add_resource("doctor", 1) %>%
-#   add_resource("consultant", 1) %>%
-#   add_resource("administration", 1) %>%
-#   add_generator("dressing patient", Dressing_Patient, function() rnorm(1, 5, 0)) %>%
-#   add_generator("return patient", Return_Patient, function() rnorm(1,1,0)) %>%
-#   add_generator("new patient", New_Patient, function() rnorm(1,2,0))
-# 
-# env %>%
-#   run(180) %>%
-#   now()
-# 
-# 
-# env %>% peek(Inf, verbose=TRUE)
 
 
-runlength <- 160
-
-envs <- mclapply(1:1000, function(i) {
+#Simulate The OPD Session
+envs <- mclapply(1:number_of_trials, function(i) {
   simmer("OPD") %>%
-    add_resource("nurse", 2) %>%
-    add_resource("doctor", 4, preemptive = TRUE) %>%
-    add_resource("consultant", 1) %>%
-    add_resource("administration", 1) %>%
-    add_generator("return", Return_Patient, from_to(c(0,0,0),runlength,
+    add_resource("nurse", Nurses) %>%
+    add_resource("doctor", Doctors, preemptive = TRUE) %>%
+    add_resource("consultant", Consultants) %>%
+    add_resource("administration", Administrators) %>%
+    add_generator("return", Return_Patient, from_to(rep(0,return_patient_first_slot),OPD_Clinic_Time,
                                               dist = function () {return_doc_time}, arrive = TRUE,
-                                              every = runlength), priority = 0) %>%
-    add_generator("new", New_Patient, from_to(c(0,0,0),runlength,
+                                              every = OPD_Clinic_Time), priority = return_priority) %>%
+    add_generator("new", New_Patient, from_to(rep(0,new_patient_first_slot),OPD_Clinic_Time,
                                               dist = function () {new_doc_time}, arrive = TRUE,
-                                              every = runlength), priority = 0) %>%
-    run(runlength) %>%
+                                              every = OPD_Clinic_Time), priority = new_priority) %>%
+    run(OPD_Clinic_Time) %>%
     wrap()
 })
 
-envs %>%
-  get_mon_arrivals(per_resource = TRUE)
 
+#View Patient Pathway
+envs %>%
+  get_mon_arrivals(per_resource = TRUE) #%>%  view()
+
+
+#Save the Results of How Many Patients were seen
 result <- envs %>% 
   get_mon_arrivals() %>%
-  transform(waiting_time = end_time - start_time - activity_time) %>%
   group_by(replication, New = str_detect(name, "new")) %>%
   tally() %>%
   pivot_wider(id_cols = replication, names_from = New , values_from = n) %>%
